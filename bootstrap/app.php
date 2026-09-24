27dc961e8384de596ba65453cc199b3c9c741923
YUSUF PC <ybasori@gmail.com>
2026-09-24 12:59:20 +0700

    add auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
