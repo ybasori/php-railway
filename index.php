a262a770cd0167c258f21b410244456dd74651ff
Yusuf <ybasori@gmail.com>
2026-09-24 11:00:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?= "halooo" ?>
+<?php
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$db   = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
